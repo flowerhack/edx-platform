50354d999e966c968bfe0ddb76426f43603de41c
Julia Hansbrough <julia@edx.org>
2013-12-12 16:03:07 +0000

    refactoring joinus because evidently django has no builtin roles

--- lms/djangoapps/joinus/models.py
+++ lms/djangoapps/joinus/models.py
@@ -7,12 +7,14 @@ class JoinUs(models.Model):
 	Models a user-created study group.
 	"""
 	JOINUS_GROUP_PREFIX = "joinus_"
+	members = models.ForeignKey(Groups)
+	leader = models.ForeignKey(User)
 
 	@classmethod
-	def join_group(cls, user, gname):
+	def join_joinus_group(cls, user, gname):
 		""" Adds user to the JoinUs Group with name gname. """
 		gname = JOINUS_GROUP_PREFIX + gname
-		group = Group.objects.get(name='gname')
+		group = JoinUs.group.objects.get(name='gname')
 		group.user_set.add(user)
 		return
 
@@ -25,29 +27,27 @@ class JoinUs(models.Model):
 		pass
 
 	@classmethod
-	def remove_user_from_group(cls, user, gname):
+	def remove_user_from_joinus_group(cls, user, gname):
 		""" 
 		Removes user from the JoinUs Group with name gname.
 		If that user is the group leader, this also deletes the group.
 		"""
 		gname = JOINUS_GROUP_PREFIX + gname
-		group = Group.objects.get(name='gname')
+		group = JoinUs.group.objects.get(name='gname')
 		group.user_set.remove(user)
 		# TODO if user is group leader, delete group
 		return
 
-	def get_group_info(self, user):
+	def get_joinus_group_info(self, user):
 		# lets a user see the code they have access to
 		pass
 
 	@classmethod
-	def create_group(cls, user, gname):
+	def create_joinus_group(cls, user, gname):
 		# creates a new group led by user with name
 		# TODO check that name is valid, not taken, etc
 		gname = JOINUS_GROUP_PREFIX + gname
-		if Group.objects.filter(name=gname):
-			# return an error
-			pass
 		group = Group(name=gname)
-		group.save()
+		joinus_group = cls.objects.create(members=group, leader=user)
+		joinus_group.save()
 		return
